93c3888ed041f18ae616f390a7942a10e562ce5c
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 14:40:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ st.markdown("""
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
+  <div class="collapse navbar-collapse mx-auto" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active" >
         <a class="nav-link disabled" href="#">Query Based Video Retrieval System <span class="sr-only">(current)</span></a>
