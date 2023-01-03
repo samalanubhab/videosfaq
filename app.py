4ee33f178d0480d71c69b51327fe8c000d77fee1
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 13:14:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ st.set_page_config(layout="wide")
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
-st.markdown("""
-<nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #3498DB;">
+st.markdown("""<nav class="navbar navbar-expand-lg navbar-light bg-dark" style="background-color: #3498DB;">
   <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -20,10 +19,7 @@ st.markdown("""
     <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a> 
-									
+      </li>		
 																																																			
 				  
 			
@@ -40,6 +36,7 @@ st.markdown("""
 		   
   </div>
 </nav>
+
 """, unsafe_allow_html=True)
 
 @st.cache(allow_output_mutation=True)
