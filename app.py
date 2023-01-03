091bff52c67771d5cd2d5e4a73d689f6a60093a4
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 13:23:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""													
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://home.kpmg/xx/en/home.html" target="_blank">APP</a>
+  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Query Based Video Retrieval <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
-      
+      <li class="nav-item">
+        <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://twitter.com/thedataprof" target="_blank">Twitter</a>
+      </li>
     </ul>
   </div>
 </nav> 
