4899f3100d23d693d0e0698fa93c784f12037e7d
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 15:06:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ with col3:
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://home.kpmg/xx/en/home.html" target="_blank">KPMG</a>
+  <a class="navbar-brand" href="https://home.kpmg/xx/en/home.html" target="_blank">Contact Us</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
