6c4ec651af47865947ec19a1b7f7c8d7a5314b35
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 12:46:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import numpy as np
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 
-st.set_page_config(layout="wide")
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
