c3c302a00501ff58af9240a49b19483407de5217
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 12:40:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,27 @@ def vector_search(query,model,index,num_results=3):
 
     
 def main():
+    st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+    st.markdown("""
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #3361FF;">
+      <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://youtube.com/dataprofessor" target="_blank">YouTube</a>
+          </li>
+         </ul>
+      </div>
+    </nav>
+    """, unsafe_allow_html=True)
+    
     html_text="""<div style="background-color:green;padding:10px">
     <h2 style="color:white;text-align:center;">Query Based Video Timsetamp Retrieval System</h2></div>    
     """
