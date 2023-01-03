6a5a5b95d5af97bf6a892fc15714233db36f6b68
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 18:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,9 +69,11 @@ import threading
 p = pyaudio.PyAudio()
 info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
+st.write("Number of devices",numdevices)
 for i in range(0, numdevices):
         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
             print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+            st.write("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
 
 # Define audio parameters
 format = pyaudio.paInt16
@@ -90,7 +92,7 @@ def start_recording():
                     channels=channels,
                     rate=rate,
                     input=True,
-                    input_device_index=0,
+                 
                     frames_per_buffer=chunk)
 
     # Start a thread to stream the audio
