b61be8d1e47cf2ea0a307ab223c4456d9193fdb1
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 10:43:27 +0530

    added code for recording and playing audio

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import faiss,pickle
 import numpy as np
 import streamlit as st
 from sentence_transformers import SentenceTransformer
+import pyaudio,wave
 
 
 
@@ -30,6 +31,78 @@ def vector_search(query,model,index,num_results=3):
 
   return D,I
 
+def record():
+    chunk = 1024  # Record in chunks of 1024 samples
+    sample_format = pyaudio.paInt16  # 16 bits per sample
+    channels = 2
+    fs = 44100  # Record at 44100 samples per second
+    seconds = 3
+    filename = "output.wav"
+
+    p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+    print('Recording')
+
+    stream = p.open(format=sample_format,
+                    channels=channels,
+                    rate=fs,
+                    frames_per_buffer=chunk,
+                    input=True)
+
+    frames = []  # Initialize array to store frames
+
+    # Store data in chunks for 3 seconds
+    for i in range(0, int(fs / chunk * seconds)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    # Stop and close the stream 
+    stream.stop_stream()
+    stream.close()
+    # Terminate the PortAudio interface
+    p.terminate()
+
+    print('Finished recording')
+
+    # Save the recorded data as a WAV file
+    wf = wave.open(filename, 'wb')
+    wf.setnchannels(channels)
+    wf.setsampwidth(p.get_sample_size(sample_format))
+    wf.setframerate(fs)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    
+    return filename
+
+def play_record(filename):
+    # Set chunk size of 1024 samples per data frame
+    chunk = 1024  
+
+    # Open the sound file 
+    wf = wave.open(filename, 'rb')
+
+    # Create an interface to PortAudio
+    p = pyaudio.PyAudio()
+
+    # Open a .Stream object to write the WAV file to
+    # 'output = True' indicates that the sound will be played rather than recorded
+    stream = p.open(format = p.get_format_from_width(wf.getsampwidth()),
+                    channels = wf.getnchannels(),
+                    rate = wf.getframerate(),
+                    output = True)
+
+    # Read data in chunks
+    data = wf.readframes(chunk)
+
+    # Play the sound by writing the audio data to the stream
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(chunk)
+
+    # Close and terminate the stream
+    stream.close()
+    p.terminate()
+    
 def main():
     html_text="""<div style="background-color:green;padding:10px">
     <h2 style="color:white;text-align:center;">Query Based Video Timsetamp Retrieval System</h2></div>    
@@ -42,6 +115,14 @@ def main():
     
     model=get_model()
     data,faiss_index=load_meta_index() 
+    
+    if st.button("Start Recording"):
+	    with st.spinner("Recording..."):
+		    filename=record()
+    
+    if st.button("Play Recording"):
+        play_record(filename)
+        
     user_query=st.text_area("Enter text", value="Default")
 
     submit=st.button("Submit")
