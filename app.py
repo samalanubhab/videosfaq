e42d37ef2d1319e0f6e29fa91c3f0c7b473f520a
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 19:30:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import faiss,pickle
 import numpy as np
 import streamlit as st
 from sentence_transformers import SentenceTransformer
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
+
 
 st.set_page_config(layout="wide")
 
@@ -84,43 +82,7 @@ def main():
     model=get_model()
     data,faiss_index=load_meta_index() 
     
-    from bokeh.models.widgets import Button
-    from bokeh.models import CustomJS
-    from streamlit_bokeh_events import streamlit_bokeh_events
-
-    stt_button = Button(label="Speak", width=100)
-
-    stt_button.js_on_event("button_click", CustomJS(code="""
-        var recognition = new webkitSpeechRecognition();
-        recognition.continuous = true;
-        recognition.interimResults = true;
-
-        recognition.onresult = function (e) {
-            var value = "";
-            for (var i = e.resultIndex; i < e.results.length; ++i) {
-                if (e.results[i].isFinal) {
-                    value += e.results[i][0].transcript;
-                }
-            }
-            if ( value != "") {
-                document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
-            }
-        }
-        recognition.start();
-        """))
-
-    result = streamlit_bokeh_events(
-    stt_button,
-    events="GET_TEXT",
-    key="listen",
-    refresh_on_update=False,
-    override_height=75,
-    debounce_time=0)
-
-    if result:
-        if "GET_TEXT" in result:
-            st.write(result.get("GET_TEXT"))
-    
+   
     user_query=st.text_area("Enter text", value="Default")
     # Add css to make text bigger
     st.markdown(
