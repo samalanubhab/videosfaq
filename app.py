54bb2e0cbc91ec69bfa84e67311acd47f4da940f
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 14:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,22 +22,16 @@ with col3:
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""													
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">My App</a>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
+  <a class="navbar-brand" href="https://home.kpmg/xx/en/home.html" target="_blank">KPMG</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav mx-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Page 1</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Page 2</a>
-      </li>
+        <a class="nav-link" href="#">Query Based Video Retrieval System</a>
+      </li>      
     </ul>
   </div>
 </nav>
