59721a189e5a6841ec9cef35fb33b8db73c72aee
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 12:50:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import numpy as np
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 
+st.markdown(""" <style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html=True)
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
