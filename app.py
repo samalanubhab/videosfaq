8074457c5fbda72fafec7196c462b25148eed382
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 20:40:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,20 +82,35 @@ def main():
     model=get_model()
     data,faiss_index=load_meta_index() 
     
-   
-    user_query=st.text_area("Enter text", value="Default")
-    # Add css to make text bigger
-    st.markdown(
-    """
-    <style>
-    textarea {
-        font-size: 2rem !important;
-    }
+    def local_css(file_name):
+        with open(file_name) as f:
+            st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+    def remote_css(url):
+        st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)    
+
+    def icon(icon_name):
+        st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
+
+    local_css("style.css")
+    remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+    icon("search")
+
+    user_query = st.text_input("", "Default")
+#     button_clicked = st.button("OK")
+#     user_query=st.text_area("Enter text", value="Default")
+#     # Add css to make text bigger
+#     st.markdown(
+#     """
+#     <style>
+#     textarea {
+#         font-size: 2rem !important;
+#     }
     
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+#     </style>
+#     """,
+#     unsafe_allow_html=True,
+# )
 
     submit=st.button("Submit")
 
