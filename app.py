764d68a8006bc2a1ca5e6e04ac3092ff7c8db920
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-03 13:25:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,18 +11,18 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""													
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3498DB;">
-  <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Data Professor</a>
+  <a class="navbar-brand" href="https://home.kpmg/xx/en/home.html" target="_blank">KPMG</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="#">Query Based Video Retrieval System <span class="sr-only">(current)</span></a>
       </li>
     </ul>
   </div>
-</nav>  
+</nav> 	
 """, unsafe_allow_html=True)
 
 @st.cache(allow_output_mutation=True)
