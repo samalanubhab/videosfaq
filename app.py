f171d20660c2a19a9a0df802b40c185cd77268fd
samalanubhab <43805889+samalanubhab@users.noreply.github.com>
2023-01-05 19:07:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def main():
 
     local_css("style.css")
     
-    col4, col5, col6 = st.columns([1,5,1])
+    col4, col5, col6 = st.columns([5,1,5])
     with col4:
         pass
     with col5:
